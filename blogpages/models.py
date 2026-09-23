c6aec5355e3bc2961a0c8d569301756071a40768
Kristian Cross <etamin1985@yahoo.com>
2026-09-23 18:05:59 +0100

    Add image model & context to blogpages, add migrations

--- blogpages/models.py
+++ blogpages/models.py
@@ -19,14 +19,26 @@ class BlogIndex(Page):
         FieldPanel("body"),
     ]
 
+    def get_context(self, request):
+        context = super().get_context(request)
+        context["blogpages"] = BlogDetail.objects.live().public()
+        return context
+
+
 
 class BlogDetail(Page):
-    template = "blogpages/blog_detail_page.html"
-
     subtitle = models.CharField(max_length=100, blank=True)
     body = RichTextField(blank=True)
     parent_page_types = ["blogpages.BlogIndex"]
     subpage_types = []
+
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
 
     content_panels = Page.content_panels + [
         FieldPanel("subtitle"),
